f6c26013c7addeb19f7cab4b2ab214c445e94d7e
Joshua Henderson <joshua.henderson@microchip.com>
2018-04-06 09:55:10 -0700

    add README and license
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+![Microchip](docs/microchip_logo.png)
+
+# Microchip 3D Parallax Demo Application
+
+This is an application that makes use of hardware LCD planes to create a parallax effect.  Several touch events are handled to make the cowboy jump or shoot.
+
+## License
+
+This project is is released under the terms of the `Apache 2.0` license. See the `COPYING` file for more information.  Some source files may be available under different licenses.
--- tools.h
+++ tools.h
@@ -1,11 +1,8 @@
 /*
- * Copyright (C) 2013-2015 Atmel Corporation.
+ * Copyright (C) 2018 Microchip Technology Inc.  All rights reserved.
+ * Joshua Henderson <joshua.henderson@microchip.com>
  *
- * This file is licensed under the terms of the Atmel LIMITED License Agreement
- * as written in the COPYING file that is delivered in the project root directory.
- *
- * DISCLAIMER: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
- * See the COPYING license file for more details.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifndef TOOLS_H
